d53c5bf202586e1ab2d12bc04067d4f5a5e097bb
khanh <khanhpzo99@gmail.com>
2022-11-29 11:32:02 +0700

    add lib

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -18,7 +18,7 @@ public class DBContext {
     /*Insert your other code right after this comment*/
  /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
     private final String serverName = "LAPTOP-9KPF5SGI\\SQLEXPRESS";
-    private final String dbName = "SWP391_ShoesStore";
+    private final String dbName = "SWP391_OnlineShop";
     private final String portNumber = "1433";
     private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
