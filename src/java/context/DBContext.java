0dc439d0882eb5153783bb82a2d5824e392c04f6
TranPhucHung <hungtphe130612@fpt.edu.vn>
2022-12-06 20:14:00 +0700

    fix conflict DBContext

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -14,7 +14,14 @@ public class DBContext {
 public Connection connection;
     /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
  /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
-    
+        public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+        if (instance == null || instance.trim().isEmpty()) {
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        }
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
     /*Insert your other code right after this comment*/
  /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
     private final String serverName = "LAPTOP-9KPF5SGI\\SQLEXPRESS";
