2acd3e4905196a3aa6f74a92f6f7938f6565fcbd
NguyenDucNgocLong <longndnhe130377@fpt.edu.vn>
2022-12-12 09:59:40 +0700

    fix conflict dbcontext

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -11,10 +11,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DBContext {
-public Connection connection;
+
+    public Connection connection;
+
     /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
  /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
-        public Connection getConnection() throws Exception {
+    public Connection getConnection() throws Exception {
         String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
         if (instance == null || instance.trim().isEmpty()) {
             url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
@@ -25,14 +27,26 @@ public Connection connection;
     /*Insert your other code right after this comment*/
  /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
 
-    private final String serverName = "LAPTOP-9KPF5SGI\\SQLEXPRESS";
-
+    private final String serverName = "DESKTOP-24MVRR0";
     private final String dbName = "Shop_Online";
     private final String portNumber = "1433";
     private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
     private final String password = "khanh123";
-    
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+            if (instance == null || instance.trim().isEmpty()) {
+                url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+            }
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, userID, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public static void main(String[] args) {
         try {
             DBContext db = new DBContext();
