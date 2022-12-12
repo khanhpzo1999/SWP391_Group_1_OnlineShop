65d84bc512b9d2af83d3f41a4247230e5c417e6c
NguyenDucNgocLong <longndnhe130377@fpt.edu.vn>
2022-12-12 11:14:40 +0700

    update ordercart

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -32,7 +32,7 @@ public class DBContext {
     private final String portNumber = "1433";
     private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
-    private final String password = "khanh123";
+    private final String password = "123";
 
     public DBContext() {
         try {
